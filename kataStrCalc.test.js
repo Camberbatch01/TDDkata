0864978edf01e4333c2fb011b7ff04a16d74dafc
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-06 16:48:22 +0000

    red

--- kataStrCalc.test.js
+++ kataStrCalc.test.js
@@ -9,13 +9,16 @@ const Add = (strNumber1, strNumber2) => {
     if (sum(strNumber1, strNumber2, "2")){
         return "2"
     }
-    return "";
+    return "0";
 }
 
 test("when no input, return empty string", () => {
-    expect(Add("", "")).toEqual("")
+    expect(Add("", "")).toEqual("0")
 })
 test("only 1 input given, return the sum", () => {
     expect(Add("", "1")).toEqual("1")
     expect(Add("", "2")).toEqual("2")
+})
+test("2 inputs, returns inputs sum", () => {
+    expect(Add("1", "2")).toEqual("3")
 })
