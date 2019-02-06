b058103a6f53bee6057f3716071cef8455b5753d
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-06 17:04:51 +0000

    green, converted to number form for calc

--- kataStrCalc.test.js
+++ kataStrCalc.test.js
@@ -1,27 +1,27 @@
 const sum = (num1, num2, value) => {
-    return num1+num2 === value;
+    return Number(num1) + Number(num2) === value;
 }
 
 const Add = (strNumber1, strNumber2) => {
-    if (sum(strNumber1, strNumber2, "1")){
-        return "1"
+    if (sum(strNumber1, strNumber2, 1)){
+        return 1
     }
-    if (sum(strNumber1, strNumber2, "2")){
-        return "2"
+    if (sum(strNumber1, strNumber2, 2)){
+        return 2
     }
-    if (sum(strNumber1, strNumber2, "3")){
-        return "3"
+    if (sum(strNumber1, strNumber2, 3)){
+        return 3
     }
-    return "0";
+    return 0;
 }
 
 test("when no input, return empty string", () => {
-    expect(Add("", "")).toEqual("0")
+    expect(Add("", "")).toEqual(0)
 })
 test("only 1 input given, return the sum", () => {
-    expect(Add("", "1")).toEqual("1")
-    expect(Add("", "2")).toEqual("2")
+    expect(Add("", "1")).toEqual(1)
+    expect(Add("", "2")).toEqual(2)
 })
 test("2 inputs, returns inputs sum", () => {
-    expect(Add("1", "2")).toEqual("3")
+    expect(Add("1", "2")).toEqual(3)
 })
